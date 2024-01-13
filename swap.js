a4690d4eea3432dd4a667ba3bfe04084c4c1cf75
Vanthana <154410882+vanthana2@users.noreply.github.com>
2024-01-13 15:58:26 +0530

    Add files via upload

---
+++ swap.js
@@ -0,0 +1,8 @@
+let a=parseInt(prompt("Enter the first number"));
+let b=parseInt(prompt("Enter the second number"));
+a=a+b;
+b=a-b;
+a=a-b;
+console.log("The value of a after swapping number is " +a);
+console.log("The value of b after swapping number is " +b);
+
